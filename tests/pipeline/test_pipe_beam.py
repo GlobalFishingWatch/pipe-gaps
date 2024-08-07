b3cfdcb7861b046b27a89457dea5679f9b01fc86
Tomás Link <tomaslink@gmail.com>
2024-08-07 20:35:59 -0300

    Add failing test that checks that pipeline handles border case

--- tests/pipeline/test_pipe_beam.py
+++ tests/pipeline/test_pipe_beam.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from pipe_gaps.pipeline import BeamPipeline
-from pipe_gaps.utils import json_save
+from pipe_gaps.utils import json_save, json_load
 
 
 def test_with_input_file(tmp_path, messages):
@@ -33,3 +33,42 @@ def test_save_json(tmp_path, messages):
     output_path = tmp_path.joinpath(output_file)
 
     assert output_path.is_file()
+
+
+def test_border_case(tmp_path):
+    # Checks that a gap between years is properly detected.
+    messages = [
+        {
+            "ssvid": "226013750",
+            "msgid": "295fa26f-cee9-1d86-8d28-d5ed96c32835",
+            "timestamp": datetime(2023, 12, 31, 23).timestamp(),
+            "distance_from_shore_m": 1
+        },
+        {
+            "ssvid": "226013750",
+            "msgid": "295fa26f-cee9-1d86-8d28-d5ed96c32835",
+            "timestamp": datetime(2023, 12, 31, 23).timestamp(),
+            "distance_from_shore_m": 1
+        },
+        {
+            "ssvid": "226013750",
+            "msgid": "295fa26f-cee9-1d86-8d28-d5ed96c32835",
+            "timestamp": datetime(2024, 1, 1, 1).timestamp(),
+            "distance_from_shore_m": 1
+        }
+    ]
+
+    input_file = tmp_path.joinpath("test.json")
+    json_save(messages, input_file)
+
+    pipe = BeamPipeline.build(
+        input_file=input_file, work_dir=tmp_path, core=dict(threshold=0.5), save_json=True
+    )
+    pipe.run()
+
+    output_file = f"beam-gaps-{input_file.stem}.json"
+    output_path = tmp_path.joinpath(output_file)
+
+    gaps = json_load(output_path, lines=True)
+
+    assert len(gaps) == 1
