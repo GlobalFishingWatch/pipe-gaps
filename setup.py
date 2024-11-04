99daea942abc2d69feb1c49eacb4c249dd6e9e4f
Tomás Link <tomaslink@gmail.com>
2024-11-04 19:04:46 -0300

    Add schemas folder to package_data

--- setup.py
+++ setup.py
@@ -15,7 +15,12 @@ setuptools.setup(
     url="https://github.com/GlobalFishingWatch/pipe-gaps",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={'pipe_gaps': ['data/*.json']},
+    package_data={
+        'pipe_gaps': [
+            'data/*.json',
+            'pipeline/schemas/*.json'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
