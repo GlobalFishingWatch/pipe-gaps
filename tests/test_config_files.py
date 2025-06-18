25210305ef30f997ad06b9cc6b1a22ca2bbb11d2
Tomás Link <tomas.link@globalfishingwatch.org>
2025-06-17 23:42:51 -0300

    Remove bq_client and old cli

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -4,16 +4,16 @@ import json
 import pytest
 
 from pipe_gaps.utils import json_load
-from pipe_gaps.cli import build_pipeline
+# from pipe_gaps.cli import build_pipeline
 
-from pydantic import ValidationError, validate_call
+# from pydantic import ValidationError, validate_call
 
 
 @pytest.mark.skip("refactoring")
 def test_config_files():
     for file in glob.glob("config/*.json"):
         try:
-            config_dict = json_load(file)
-            validate_call(build_pipeline)(**config_dict)
-        except (json.decoder.JSONDecodeError, ValidationError) as e:
+            _ = json_load(file)
+            # validate_call(build_pipeline)(**config_dict)
+        except (json.decoder.JSONDecodeError) as e:
             raise Exception(f"Error in {file}: {e}")
